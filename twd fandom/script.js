c76db8dde9fdf5bfb62d46b0cb33c591dda33c54
Abdus9012 <94848680+Abdus9012@users.noreply.github.com>
2022-08-13 15:56:55 +0530

    Add files via upload

---
+++ twd fandom/script.js
@@ -0,0 +1,6 @@
+let details= document.querySelector('button')
+details.addEventListener('click', showMsg)
+
+function showMsg(){
+    alert("This is a frontend web development project made for educational purposes. All the copyrights of the images used belong to AMC network. The text used in this website is taken from Wikipedia. No monetized profit is intended to claim thorugh this work.")
+}
